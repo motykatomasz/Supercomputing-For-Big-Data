cfd3631a62c101bd615c886ce7bc11aee3de3ea8
Tomasz <motyka.tomasz.1995@gmail.com>
2019-09-13 20:15:20 +0200

    [LAB 1] Refactor RDD implementation

--- lab1/src/main/scala/RddBasedImplementation.scala
+++ lab1/src/main/scala/RddBasedImplementation.scala
@@ -1,35 +1,38 @@
+import java.text.SimpleDateFormat
+
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.SparkSession
 
 object RddBasedImplementation {
+
   def main(args: Array[String]) {
+
     Logger.getLogger("org.apache.spark").setLevel(Level.WARN)
 
     val spark = SparkSession.builder.appName("Lab 1 RDD implementation").config("spark.master", "local").getOrCreate()
-
     val sc = spark.sparkContext
 
-    val stringRDD = sc.textFile("./data/segment/*.gkg.csv")
+    val rawRDD = sc.textFile("./data/segment/*.gkg.csv")
 
-    val splittedColumns = stringRDD.map(_.split("\t",-1))
+    val splittedColumns = rawRDD.map(_.split("\t",-1))
 
-    val filteredColumnsAndNamesSpiltted = splittedColumns.map(row => (row(1), row(23).split(";")))
+    val filteredColumns = splittedColumns.map(row => (row(1), row(23).split(";")))
 
-    val groupedByDate = filteredColumnsAndNamesSpiltted.reduceByKey((x,y) => x ++ y)
+    val splittedNamesColumn = filteredColumns.mapValues(names => names.map(_.split(",")))
 
-    val mappedValues = groupedByDate.mapValues(names => names.map(_.split(",")))
+    val addedOnesToNames = splittedNamesColumn.flatMapValues(a => a.filter(a => a(0) != "").map(a => (a(0), 1)))
 
-    val addedOnes = mappedValues.flatMapValues(a => a.map(a => (a(0), 1)))
-
-    val groupedByDateAndName = addedOnes.map{ case (date: String, (name: String, count: Int)) =>
+    val preparedForCounting = addedOnesToNames.map{ case (date: String, (name: String, count: Int)) =>
       ((date, name), count)
     }
 
-    val countedByDate = groupedByDateAndName.reduceByKey(_+_).map{ case ((date: String, name: String), count: Int) =>
-      (date, (name, count))
+    val dateFormat = new SimpleDateFormat("yyyyMMddHHmmss")
+
+    val countedNamesByDate = preparedForCounting.reduceByKey(_+_).map{ case ((date: String, name: String), count: Int) =>
+      (dateFormat.parse(date), (name, count))
     }.groupByKey()
 
-    val sorted = countedByDate.mapValues(l => l.toList.sortBy(_._2)(Ordering[Int].reverse).take(10))
+    val sorted = countedNamesByDate.mapValues(l => l.toList.sortBy(_._2)(Ordering[Int].reverse).take(10))
 
     sorted.foreach(println)
 
