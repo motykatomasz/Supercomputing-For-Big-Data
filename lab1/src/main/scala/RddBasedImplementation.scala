0723f1059f9b050e57e514c139ba17b30ad9b4f1
Tomasz <motyka.tomasz.1995@gmail.com>
2019-09-13 19:38:26 +0200

    [Lab 1] First working rdd implementation

--- lab1/src/main/scala/RddBasedImplementation.scala
+++ lab1/src/main/scala/RddBasedImplementation.scala
@@ -7,7 +7,31 @@ object RddBasedImplementation {
 
     val spark = SparkSession.builder.appName("Lab 1 RDD implementation").config("spark.master", "local").getOrCreate()
 
-    // ...
+    val sc = spark.sparkContext
+
+    val stringRDD = sc.textFile("./data/segment/*.gkg.csv")
+
+    val splittedColumns = stringRDD.map(_.split("\t",-1))
+
+    val filteredColumnsAndNamesSpiltted = splittedColumns.map(row => (row(1), row(23).split(";")))
+
+    val groupedByDate = filteredColumnsAndNamesSpiltted.reduceByKey((x,y) => x ++ y)
+
+    val mappedValues = groupedByDate.mapValues(names => names.map(_.split(",")))
+
+    val addedOnes = mappedValues.flatMapValues(a => a.map(a => (a(0), 1)))
+
+    val groupedByDateAndName = addedOnes.map{ case (date: String, (name: String, count: Int)) =>
+      ((date, name), count)
+    }
+
+    val countedByDate = groupedByDateAndName.reduceByKey(_+_).map{ case ((date: String, name: String), count: Int) =>
+      (date, (name, count))
+    }.groupByKey()
+
+    val sorted = countedByDate.mapValues(l => l.toList.sortBy(_._2)(Ordering[Int].reverse).take(10))
+
+    sorted.foreach(println)
 
     spark.stop()
   }
